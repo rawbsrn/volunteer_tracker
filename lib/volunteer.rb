0640f13c9fc420200e39a48f3040ed242626003b
Rob Sameh <rob.sameh@gmail.com>
2022-05-19 16:37:46 -0700

    .all volunteer test passing

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -16,4 +16,22 @@ class Volunteer
     end
   end
 
+  def self.all
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => project_id, :id => id}))
+    end
+    volunteers
+  end
+
+  
+  def save
+    result = DB.exec("INSERT INTO volunteers (name, project_id) VALUES ('#{@name}', #{@project_id}) RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+
 end
