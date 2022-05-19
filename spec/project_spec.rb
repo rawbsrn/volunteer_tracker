29d2d05601c1693c35cb7824d3e1b6aa555d0211
Rob Sameh <rob.sameh@gmail.com>
2022-05-19 15:26:54 -0700

    title test passing

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -1,12 +1,12 @@
 require "spec_helper"
 
 describe Project do
-  # describe '#title' do
-  #   it 'returns the project title' do
-  #     project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
-  #     expect(project.title).to eq 'Teaching Kids to Code'
-  #   end
-  # end
+  describe '#title' do
+    it 'returns the project title' do
+      project = Project.new({:title => 'Teaching Kids to Code', :id => nil})
+      expect(project.title).to eq 'Teaching Kids to Code'
+    end
+  end
 
   # context '#id' do
   #   it 'returns the id of the project before saving project' do
