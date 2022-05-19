cbf83d37950a23a83f26e1187819f4d66405442a
Rob Sameh <rob.sameh@gmail.com>
2022-05-19 16:29:12 -0700

    first three volunteer tests passing

---
+++ lib/volunteer.rb
@@ -0,0 +1,19 @@
+class Volunteer
+  attr_reader :id
+  attr_accessor :name, :project_id
+
+  def initialize(attributes)
+    @name = attributes.fetch(:name)
+    @project_id = attributes.fetch(:project_id)
+    @id = attributes.fetch(:id)
+  end
+
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.name() == volunteer_to_compare.name()) && (self.project_id() == volunteer_to_compare.project_id())
+    else
+      false
+    end
+  end
+
+end
